2d50b7e47a7394bfaed6b7016cfbe90ac4d90f03
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-08 08:22:01 +0200

    deleted some files

--- react-d01/bulkImages.js
+++
@@ -1,8 +0,0 @@
-const bulkImages = [
-    'images/eliza_arzanukaeva/image4.jpg',
-    'images/eliza_arzanukaeva/image2.jpg',
-    'images/eliza_arzanukaeva/image3.jpg',
-    'images/eliza_arzanukaeva/image1.jpg'
-];
-
-export default bulkImages;
