86c3aa98053e5a6dd753fcea51b88cc4f9cf98ae
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-25 19:05:52 +0200

    added package.json

--- JS/backend/server.js
+++ JS/backend/server.js
@@ -18,3 +18,23 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`listening on port http://localhost:${port}`);
 });
+
+
+
+// {
+//   "name": "backend2",
+//   "version": "1.0.0",
+//   "description": "",
+//   "main": "backend.js",
+//   "type": "module",
+//   "scripts": {
+//     "backend": "nodemon server.js"
+//   },
+//   "keywords": [],
+//   "author": "",
+//   "license": "ISC",
+//   "dependencies": {
+//     "express": "^4.17.1",
+//     "nodemon": "^2.0.14"
+//   }
+// }
