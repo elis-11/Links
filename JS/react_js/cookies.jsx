8f895891071e9429537be57cc90532af611657bf
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-27 18:01:03 +0100

    added some info

--- JS/react_js/cookies.jsx
+++
@@ -1,8 +0,0 @@
-export const checkLS = () => {
-	if (localStorage.getItem('w')) return true;
-	return false;
-};
-
-export const showMessage = (text) => {
-	return`${text}! Message not found`;
-};
--- JS/react_js/script.jsx
+++
@@ -1,9 +0,0 @@
-import otmazka from 'otmazka';
-
-console.log(otmazka('instruction'));
-console.log(otmazka('assessment'));
-console.log(otmazka('tascs'));
-
-import { checkLS, showMessage } from './cookies';
-if (!checkLS()) console.log(showMessage('problem'));
-
