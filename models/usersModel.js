df7fe0c1adaf8600b7e5696116124bceda243ee4
elis-11 <eliza.arzanukaeva@gmail.com>
2022-08-05 09:44:31 +0200

    Modified Users Part

--- models/usersModel.js
+++ models/usersModel.js
@@ -4,9 +4,7 @@ const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  profession: { type: String, required: true },
   hobby: { type: String, required: true },
-  email: { type: String, required: true },
   city: { type: String, required: true },
 });
 
